f914af09cc30c2f6fce65b6bbdb76d6c29d4f2ee
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-18 17:45:24 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turn = 0
-  while turn < 9
-  turn(board)
-  turn += 1
+  counter = 0
+  while counter < 9
+  counter(board)
+  counter += 1
   end
 end
